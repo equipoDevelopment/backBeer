2d34780538797b47c7f8bd66ecc5875373a012bd
BadKernel <106186643+DevBadKernel@users.noreply.github.com>
2023-02-14 17:46:18 +0100

    Update config.js

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const config = {
 
   port: 3000,
-  uri: `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@clusterbadkernel.tphaiss.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`,
+  uri: `mongodb+srv://BadKernel:zUduCAwBCfrW2PWf@clusterbadkernel.tphaiss.mongodb.net/beer_store?retryWrites=true&w=majority`,
   uri_local: `mongodb://${process.env.USER_LOCAL}:${process.env.PASSWORD_LOCAL}localhost:27017/${process.env.DBNAME}`,
   uri_localNoPassword: `mongodb://localhost:27017/${process.env.DBNAME}`,
   secret : process.env.SECRET,
