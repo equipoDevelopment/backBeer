e429806582942a365753d854fb245e7789d78eca
David Maza <david.maza@hotmail.es>
2023-02-15 10:15:24 +0100

    Debugging the code �

--- src/services/productsModel.js
+++ src/services/productsModel.js
@@ -21,9 +21,28 @@ exports.edit_product = async (id,body) => {
   return await Product.findByIdAndUpdate(id, body);
 }
 
+
+//add for home and store
 exports.get_brand  = async (brand) => {
-  return await Product.find().where('total').gt(0);
+  return await Product.find({brand}).where('total').gt(0);
 }
+
+exports.get_category  = async (category) => {
+  return await Product.find({category}).where('total').gt(0);
+}
+
+exports.get_graduation  = async (graduation) => {
+  return await Product.find({graduation}).where('total').gt(0);
+}
+
+exports.get_score  = async (score) => {
+  return await Product.find({score}).where('total').gt(0);
+}
+
+exports.get_price  = async (price) => {
+  return await Product.find({price}).where('total').gt(0);
+}
+
 
 exports.get_novelty = async () => {
   function getRandomInt(max) {
